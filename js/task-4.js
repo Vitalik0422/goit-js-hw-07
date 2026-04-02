8dd0550ca9d34da9bbe5f93098f81b97fe2f4440
Vitalik0422 <vitaliy19980422@gmail.com>
2026-04-02 11:46:36 +0300

    Fixed validation: added trim() to prevent empty strings from passing

--- js/task-4.js
+++ js/task-4.js
@@ -3,14 +3,15 @@ const formLogin = document.querySelector(".login-form");
 formLogin.addEventListener("submit", (e) => {
   e.preventDefault();
   const { email, password } = e.target.elements;
-  if (!email.value || !password.value) {
-    alert("All form fields must be filled in");
-    return;
-  }
   const data = {
     email: email.value.trim(),
     password: password.value.trim(),
   };
+  if (!data.email || !data.password) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
   console.log(data);
   formLogin.reset();
 });
